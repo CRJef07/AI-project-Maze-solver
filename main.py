0546a23bc9c67da4262960c01bd2b4451f7f9dab
Jefté Andrés Vega Hidalgo <jefte.vega.hidalgo@est.una.ac.cr>
2023-09-08 22:32:47 -0600

    Create main.py
    Main file with the menu to select the algorithms

---
+++ main.py
@@ -0,0 +1,59 @@
+import os
+import time
+import psutil
+
+import bfs
+import dijkstra
+import a_star
+
+
+def limpiar_consola():
+    if os.name == 'posix':  # For Unix and Linux
+        os.system('clear')
+    elif os.name == 'nt':   # For Windows
+        os.system('cls')
+
+
+def menu():
+    print("Bienvenido!")
+    print("1. Algoritmo BFS")
+    print("2. Algoritmo Dijkstra")
+    print("3. Algoritmo A*")
+    print("77. Salir")
+
+def opcion_seleccionada():
+    opc = input("Ingrese la opcion que desea: ")
+    return opc
+
+
+def main():
+    while True:
+        limpiar_consola()
+        menu()
+        opc = opcion_seleccionada()
+
+        if opc == "1":#BFS
+            bfs.ejecutar()
+            os.system("pause")
+
+        elif opc == "2":#Dijkstra
+            dijkstra.ejecutar()
+            os.system("pause")
+
+        elif opc == "3":#A*
+            a_star.ejecutar()
+            os.system("pause")
+
+        elif opc == "77":
+            print("¡¡Hasta luego!!")
+            os.system("pause")
+            break
+
+        else:
+            print("Debe ingresar una opción válida!")
+            os.system("pause")
+
+
+if __name__ == '__main__':
+    main()
+
